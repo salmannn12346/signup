e6f02f51db6471b5941c924eceb9158a7109f7e4
salmannn12346 <meppalasalmaaan@gmail.com>
2025-06-27 17:15:30 +0530

    login done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:signup/signn.dart';
-
+import 'package:signup/Signupp.dart';
+import 'package:signup/login.dart';
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(debugShowCheckedModeBanner: false,
+    home:MyApp(),
+  ));
 }
--- lib/signn.dart
+++ lib/signn.dart
@@ -1,27 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:signup/loggin.dart';
-
-class MyApp extends StatelessWidget {
+import 'package:signup/Signupp.dart';
+import 'package:signup/login.dart';
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
-      home: Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(children: [
           Text("welcome",style: TextStyle(color: Colors.black,fontSize:50 ,fontWeight: FontWeight.bold),)  ,
           SizedBox(height: 50,),
           Image(image: NetworkImage("https://cdn.dribbble.com/userupload/23005716/file/original-36c675c448e2b5e5efb2a6ad090da9a3.png?resize=752x&vertical=center"),fit: BoxFit.fill,width: 600,),
           SizedBox(height: 100,),
-        Container(width:500,height:60,child: TextButton(onPressed:(){}, child: Text("Login",style: TextStyle(color: Colors.black,fontSize: 18),) ,) ,decoration: BoxDecoration(border: Border.all(width: 3),borderRadius: BorderRadiusGeometry.all(Radius.circular(40))),),
+          Container(width:500,height:60,child: ElevatedButton(onPressed:(){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginUp()));
+          }, child: Text("Login",style: TextStyle(color: Colors.black,fontSize: 18),) ,style: ElevatedButton.styleFrom(backgroundColor: Colors.grey,elevation:8,side: BorderSide(width: 2,),foregroundColor: Colors.white),) ,),
           SizedBox(height: 20,),
-          Container(width:500,height:60,child: TextButton(onPressed:(){}, child: Text("Sign up",style: TextStyle(color: Colors.white,fontSize: 18),) ,) ,decoration: BoxDecoration(color:Colors.indigo,borderRadius: BorderRadiusGeometry.all(Radius.circular(40))),),
+          Container(width:500,height:60,child: ElevatedButton(onPressed:(){
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>SignUp()));
+          }, child: Text("Sign up",style: TextStyle(color: Colors.white,fontSize: 18),) ,style: ElevatedButton.styleFrom(backgroundColor: Colors.blueAccent,elevation: 8,foregroundColor: Colors.white),) ,),
 
         ],),
       ),
-      ),
-    );
+      );
+
   }
 }
 
